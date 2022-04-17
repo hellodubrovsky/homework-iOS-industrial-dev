8ba80ab2382032b79fe55a321c66bd9d75819bbb
hellodubrovsky <iapopko1@mts.ru>
2022-04-18 01:19:00 +0300

    [5I] Сортировка файлов в проекте. Улучшение не касает ДЗ.

---
+++ Navigation/FeedView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedView.swift
+//  Navigation
+//
+//  Created by Илья on 18.04.2022.
+//
+
+import Foundation
+import UIKit
+
+class FeedView: UIView {
+    
+    override init() {
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
