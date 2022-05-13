7e2bed8217f7c0b268254c5f905c5fed009a3ba0
hellodubrovsky <iapopko1@mts.ru>
2022-05-13 15:44:11 +0300

    [6l] Реализовал класс кастомной кнопки.

---
+++ Navigation/Navigation/CustomButton.swift
@@ -0,0 +1,39 @@
+//
+//  CustomButton.swift
+//  Navigation
+//
+//  Created by Илья on 13.05.2022.
+//
+
+import Foundation
+import UIKit
+
+final class CustomButton: UIButton {
+    
+    // MARK: - Private Properties
+    private var buttonAction: () -> Void
+    
+    
+    
+    // MARK: - Init
+    
+    init(title: String, titleColor: UIColor, buttonAction: @escaping () -> Void) {
+        self.buttonAction = buttonAction
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    required init?(coder: NSCoder) {
+        return nil
+    }
+    
+    
+    
+    // MARK: - Private Methods
+    
+    @objc private func buttonTapped() {
+        buttonAction()
+    }
+}
