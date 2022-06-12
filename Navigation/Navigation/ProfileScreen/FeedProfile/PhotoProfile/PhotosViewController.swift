2b55b8e2bc3170b2896ff4f4c6f5fb303ccdf775
hellodubrovsky <iapopko1@mts.ru>
2022-06-12 10:55:33 +0300

    [8l] Дополнительно. Добавлен индикатор загрузки, в окне отображения фотографий.

--- Navigation/Navigation/ProfileScreen/FeedProfile/PhotoProfile/PhotosViewController.swift
+++ Navigation/Navigation/ProfileScreen/FeedProfile/PhotoProfile/PhotosViewController.swift
@@ -14,6 +14,7 @@ final class PhotosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         settingView()
+        self.activityIndicator.startAnimating()
         downloadPhotos()
     }
     
@@ -32,6 +33,12 @@ final class PhotosViewController: UIViewController {
         return collectionView
     }()
     
+    private let activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .medium)
+        indicator.color = .black
+        return indicator
+    }()
+    
     
     
     // MARK: - Private methods
@@ -45,6 +52,7 @@ final class PhotosViewController: UIViewController {
             let executionTime = methodFinish.timeIntervalSince(methodStart as Date)
             print("Время выполнения метода отображения коллекции фото: \(executionTime)")
             DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
                 self.collectionView.reloadData()
             }
         }
@@ -65,6 +73,7 @@ final class PhotosViewController: UIViewController {
         title = "Photo Gallery"
         view.backgroundColor = .systemGray
         view.addSubview(collectionView)
+        view.addSubview(activityIndicator)
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: PhotosCollectionViewCell.identifier)
@@ -73,11 +82,16 @@ final class PhotosViewController: UIViewController {
     
     private func installinConstraints() {
         collectionView.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
 }
